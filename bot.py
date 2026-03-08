d0dc6ba8dd60c007f6d4a08f376a88136d08c4c1
kingofall098 <joewad@indogmail.com>
2026-03-09 00:18:28 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ from psycopg2.pool import SimpleConnectionPool
 from telebot.apihelper import ApiTelegramException
 # ================= CONFIG ================= #
 
-BOT_TOKEN = "7858154079:AAFfAPLPt9cXk3ztcSx1XmrDCn-57zVy9wM"
+BOT_TOKEN = "8507190616:AAF4RtM6S-rzTzGvcbVxo6QsxZVNBEXu6s0"
 # DATABASE_URL = "YOUR_POSTGRES_URL"
 DATABASE_URL = os.getenv("DATABASE_URL")
 # ================= DATABASE POOL ================= #
@@ -1943,3 +1943,4 @@ if __name__ == "__main__":
     
     print("Bot is running...")
     bot.infinity_polling(skip_pending=True)
+
